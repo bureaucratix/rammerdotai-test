37e4495b313c5ff636661631275a05d7d9a12643
Bureaucratix <bureaucratix@gmail.com>
2020-03-05 14:00:02 -0800

    twilio sms setup

--- send_sms.js
+++ send_sms.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 const accountSid = process.env.TWILIOACCTSID;
 const authToken = process.env.TWILIOAUTH;
 const client = require('twilio')(accountSid, authToken);
+const testConvId = '5168539509981184';
 
-client.messages
-  .create({
-     body: 'Testing message from Twilio!',
-     from: '+13238709251',
-     to: '+12066046565'
-   })
-  .then(err => console.log(err))
-  .then(message => console.log(message.sid));
+//function that sends the text message. placeholder information now, will replace with variables when able
+const sendMessage = () => {
+
+    client.messages
+        .create({
+            body: "<Message Body>",
+            from: '+13238709251',
+            to: '+12066046565'
+        })
+        .then(message => console.log(message.sid));
+}
+
